fd9b34f56bb08d1b41133d4bbc0c8142b713b645
Rohitharish <rohitharish276@gmail.com>
2024-04-05 13:21:44 +0530

    fixed smooth scrolling

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,24 @@
-import { Montserrat } from 'next/font/google';
-import './globals.css'
-import Nav from './Navbar'
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import Nav from "./Navbar";
+import SmoothScrolling from "./(components)/SmoothScrolling";
 
-const montserrat = Montserrat({ subsets: ['latin'] });
-
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Portfolio',
-  description: ' By rh',
-}
+  title: "Portfolio",
+  description: " By rh",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <body className={montserrat.className}>
-        <Nav/>
-        {children}</body>
+      <body className={montserrat.className}>
+        <SmoothScrolling>
+          <Nav />
+          {children}
+        </SmoothScrolling>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,37 +1,16 @@
-"use client"
-import React, { useRef, useEffect } from 'react';
-import Link from 'next/link';
-import Nav from './Navbar';
-import Intro from './intro/page';
-import Lenis from '@studio-freight/lenis';
+"use client";
+import React, { useRef, useEffect } from "react";
 
-
-
+import Intro from "./intro/page";
 
 function Home() {
- 
-  useEffect(() => {
-    const lenis = new Lenis();
-  
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-  
-    requestAnimationFrame(raf);
-  
-    
-   
-  }, []); 
-  
+
+
   return (
     <main className="flex flex-col mx-2 relative">
-      
-      <section  className="bg-zinc-800 bg-opacity-60 h-screen rounded-lg my-1 relative">
+      <section className="bg-zinc-800 bg-opacity-60 h-screen rounded-lg my-1 relative">
         <Intro />
       </section>
-
-      
     </main>
   );
 }
